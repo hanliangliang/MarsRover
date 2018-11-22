199a935127b0694664158fb780bbd4bf77a30bc4
light <hanliangliang@live.com>
2018-11-22 23:28:46 +0800

    Add files via upload
    I didn't really understand the machanism of version control of git, but I just try to use it as a traditionnal way

---
+++ marsrover_.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1542865788513" ID="ID_1265064382" MODIFIED="1542865788513" TEXT="&#x65b0;&#x5efa;&#x601d;&#x7ef4;&#x5bfc;&#x56fe;"/>
+</map>
